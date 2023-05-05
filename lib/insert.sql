13288c366335796551b0ebee442ba74500422463
Corey Sawin <sawincp@gmail.com>
2023-05-05 13:15:08 -0400

    commit tree f3256eaa9fa2222f27f8e2b96c9951f5c7db7c8b

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 10, "M", "Brown", "Modest", 0);
+INSERT INTO bears VALUES (2, "Rowdy", 20, "M", "Brown", "Agressive", 1);
+INSERT INTO bears VALUES (3, "Tabitha", 3, "F", "Black", "Relaxed", 0);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 15, "M", "Black", "Modest", 1);
+INSERT INTO bears VALUES (5, "Melissa", 7, "F", "Brown", "Relaxed", 1);
+INSERT INTO bears  VALUES (6, "Grinch", 12, "M", "Black", "Agressive", 1);
+INSERT INTO bears  VALUES (7, "Wendy", 7, "F", "Brown", "Relaxed", 0);
+INSERT INTO bears  VALUES (8, NULL, 3, "F", "Black", "Modest", 0);
